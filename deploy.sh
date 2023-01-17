f44a4059bce7e0860a7c1423e59519a3bcc57b15
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-01-17 12:14:25 -0700

    updated kubernetes-secret-generator
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-helm upgrade -n argocd --install argo-cd charts/argo-cd/ -f ./charts/argo-cd/values.yaml
+kubectl get ns/argocd &>/dev/null || kubectl create ns argocd
+# Deploy argo-cd
+helm list | grep -q argo-cd || helm upgrade -n argocd --install argo-cd charts/argo-cd/ -f ./charts/argo-cd/values.yaml
+
+argo_args="--port-forward --port-forward-namespace argocd --plaintext"
+# Login with argocd CLI
+argocd login --username admin --password $(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) ${argo_args}
+# Create argo project
+argocd proj list ${argo_args} | grep -q security || argocd proj create security -d https://kubernetes.default.svc,security,argocd -s https://github.com/heywoodlh/k8s-security.git ${argo_args} --allow-cluster-resource '*/*' --allow-namespaced-resource '*/*' 
+
+# Create argo bootstrap-app
+argocd app list ${argo_args} | grep -q bootstrap-app || argocd app create bootstrap-app --project security --repo https://github.com/heywoodlh/k8s-security.git --path apps --dest-server https://kubernetes.default.svc --dest-namespace security --sync-policy automatic ${argo_args}
